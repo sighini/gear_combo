eb029afdf5cee6fdfbef76772242c582d9945e02
sighini <sighini@gmail.com>
2018-05-16 15:47:57 -0500

    Update Bicyle.java

--- Bicyle.java
+++ Bicyle.java
@@ -7,18 +7,26 @@ class Bicycle {
 		int[] f_cogs = {38, 30};
 		int[] r_cogs = {28, 23, 19, 16};
 		final float ratio = 1.6f;
+		float result_ratio = 0;
+		int result_f = 0;
+		int result_r = 0;
 		
 		DecimalFormat df = new DecimalFormat("#.#");
 		df.setRoundingMode(RoundingMode.CEILING);
-		//df.setRoundingMode(RoundingMode.DOWN);
 		for (int i = 0; i < f_cogs.length; i++) {
 			for (int j=0; j < r_cogs.length; j++) {
 				float r = ((float) f_cogs[i] )/r_cogs[j];
-				//System.out.println("Front: " + f_cogs[i] + ", Rear: " + r_cogs[j] + ", Ratio " + r + " Formatted=" + df.format(r));
-				if (ratio == Float.parseFloat(df.format(r))) {
-					System.out.println("Front: " + f_cogs[i] + ", Rear: " + r_cogs[j] + ", Ratio " + new DecimalFormat("#.###").format(r) );
+				if (r <= ratio) {
+					if (r > result_ratio) {
+						result_ratio = Float.parseFloat(new DecimalFormat("#.###").format(r));
+						result_f = f_cogs[i];
+						result_r = r_cogs[j];
+					}
 				}
 			}
 		}
+
+		System.out.println("Front: " + result_f + ", Rear: " + result_r + ", Ratio " + result_ratio );
+
 	}
 }
